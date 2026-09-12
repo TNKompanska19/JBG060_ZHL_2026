a88554e94e4fd2f5a3d7d83fa66d15a4ad3fa237
vijfsnippervijf <vijfsnippervijf@gmail.com>
2026-09-12 14:44:47 +0200

    Added linear regression to get weights

--- flooddata.py
+++ flooddata.py
@@ -1,10 +1,11 @@
+from email.mime import base
 
 import pandas as pd
-import xarray as xr
-import io
-import numpy as np
-from pathlib import Path
-from tqdm import tqdm
+import os
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LinearRegression
+
 
 #Loading the data first.
 def load_data():
@@ -16,12 +17,22 @@ def load_data():
     return data_per_item
 
 data_per_item = load_data()
+date = data_per_item['100205']["Date"]
 #Having loaded the files, I now set X and Y values:
-Y = data_per_item["100205"]['Discharge (m3/s)']
-data_per_item.pop("100205")
-X_pre: dict={}
+data_first = {}
 for i in data_per_item.keys():
-    X_pre[i] = data_per_item[i]["Discharge (m3/s)"]
-
-X = pd.DataFrame(X_pre)
-print(X)
+    data_first[i] = data_per_item[i]['Discharge (m3/s)']
+data = pd.DataFrame(data_first)
+data_clean = data.dropna()
+Y = data_clean['100205']
+X = data_clean.drop('100205', axis=1)
+assert len(X) == len(Y)
+#Linear regression and weights comparison of all Sudanese/Ethiopian stations to the one in South Sudan
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+assert len(X_train) == len(Y_train)
+assert len(X_test) == len(Y_test)
+linear_regression = LinearRegression()
+linear_regression.fit(X_train, Y_train)
+print("score: " + str(linear_regression.score(X_test, Y_test)))
+weights = linear_regression.coef_
+print(weights)
