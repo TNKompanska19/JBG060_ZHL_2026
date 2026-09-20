caadfd7cd16f13ae0186bd57fec653c6aecc31ae
vijfsnippervijf <vijfsnippervijf@gmail.com>
2026-09-20 15:33:40 +0200

    File test

--- geotiff_parquet.py
+++ geotiff_parquet.py
@@ -4,4 +4,5 @@ raster_path = 'raw_data/farmland/asap_mask_rangeland_v04.tif'
 with rasterio.open(raster_path) as src:
     print(src.meta)  # Metadata (CRS, bounds, etc.)
     data = src.read()  # Pixel data as numpy array
-print(data)
+print("Min:", data.min(), "Max:", data.max(), "Mean:", data.mean())
+print("NaN count:", np.isnan(data).sum())  # If float type
