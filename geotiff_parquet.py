637c95cd0d4c0498f1265003cc40de2e77586dd8
vijfsnippervijf <vijfsnippervijf@gmail.com>
2026-09-20 14:57:35 +0200

    Created geotiff_parquet

---
+++ geotiff_parquet.py
@@ -0,0 +1,7 @@
+import rasterio
+import numpy as np
+raster_path = 'raw_data/farmland/asap_mask_rangeland_v04.tif'
+with rasterio.open(raster_path) as src:
+    print(src.meta)  # Metadata (CRS, bounds, etc.)
+    data = src.read()  # Pixel data as numpy array
+print(data)
